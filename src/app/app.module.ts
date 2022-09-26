dec47349fdd5da28c0f40d498b11645d3993bd8a
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-26 13:57:32 +0200

    03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Store } from 'store';
 
 // feature modules
+import { AuthModule } from '../../src/auth/auth.module';
 
 // containers
 import { AppComponent } from './containers/app/app.component';
@@ -17,7 +18,8 @@ export const ROUTES: Routes = [];
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AuthModule
   ],
   declarations: [
     AppComponent
@@ -30,3 +32,30 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYqaKLFPEsbsk1NB2PAYLpEhsfS54a7ko",
+    authDomain: "fitenss-app.firebaseapp.com",
+    databaseURL: "https://fitenss-app-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fitenss-app",
+    storageBucket: "fitenss-app.appspot.com",
+    messagingSenderId: "218279070989",
+    appId: "1:218279070989:web:733f17082b945059a0f444",
+    measurementId: "G-Y7LSQRTGSQ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
+*/
